3a03a1f4a58f12aa0e7f0c7a6c967e0890c9fddd
luongkhdang <luongkhdang@gmail.com>
2024-06-23 09:31:52 -0700

    DAY00 - THE SETUP
    DAY 00  OF MY JOURNEY TO BECOME AN EMPLOYABLE SWE.

---
+++ Python Full Couse for Beginners - Programmming with Mosh/FUNDAMENTAL - LECTURES/DAY00/project 03/app04.py
@@ -0,0 +1,32 @@
+course = 'Python for Beginners'
+
+# '' and "" ; what if we need to use 's in the middle of the string
+
+course2 = "Mosh's Python for Beginners course"
+
+course3 = 'Python for "Beginners"'
+
+print(course)
+print(course2)
+print(course3)
+
+#''' '''
+course4 = '''     
+Thank you,
+The support team
+
+'''
+print(course4)
+
+
+course5 = 'Python for Beginners'
+#Python for Beginners
+#01234567890123456789
+
+print(course5[1])
+print(course5[-2])
+print(course5[5:12])  #will not print 12, start at 5
+print(course5[5:])
+
+
+
